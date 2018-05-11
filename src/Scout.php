dc34f380b256561a5d8122a34c9eaa4d686779f6
Kevin Cheng <kevincheng@kkbox.com>
2018-05-11 12:00:52 +0800

    Extract report function to a new class Staff

--- src/Scout.php
+++ src/Scout.php
@@ -34,64 +34,6 @@ class Scout
             }
         }
         curl_close($curl_client);
-        return $this->createReport($arr);
-    }
-
-    protected function createReport($arr)
-    {
-        $len = [
-            'name' => 4,
-            'watch' => 5,
-            'star' => 4,
-            'fork' => 4,
-        ];
-        foreach ($arr as $repo_data) {
-            if ($len['name'] < strlen($repo_data['name'])) {
-                $len['name'] = strlen($repo_data['name']);
-            }
-            $keys = ['watch', 'star', 'fork'];
-            foreach ($keys as $key) {
-                $column_len = strlen(sprintf("%d", $repo_data[$key]));
-                $len[$key] = max($len[$key], $column_len);
-            }
-        }
-        $border_format = sprintf(
-            "+%%%ds+%%%ds+%%%ds+%%%ds+\n",
-            $len['name'] + 2,
-            $len['watch'] + 2,
-            $len['star'] + 2,
-            $len['fork'] + 2
-        );
-        $field_format = sprintf(
-            "| %%-%ds | %%%ds | %%%ds | %%%ds |\n",
-            $len['name'],
-            $len['watch'],
-            $len['star'],
-            $len['fork']
-        );
-        
-        $border = sprintf(
-            $border_format,
-            str_repeat("-", $len['name'] + 2),
-            str_repeat("-", $len['watch'] + 2),
-            str_repeat("-", $len['star'] + 2),
-            str_repeat("-", $len['fork'] + 2)
-        );
-
-        $table = "";
-
-        $table .= "```";
-        $table .= $border;
-        $table .= sprintf($field_format, "repo", "watch", "star", "fork");
-        $table .= $border;
-
-        foreach ($arr as $repo_data) {
-            $table .= sprintf($field_format, $repo_data['name'], $repo_data['watch'], $repo_data['star'], $repo_data['fork']);
-        }
-
-        $table .= $border;
-        $table .= "```";
-
-        return $table;
+        return $arr;
     }
 }
--- src/main.php
+++ src/main.php
@@ -1,6 +1,7 @@
 <?php
 require __DIR__ . '/Messenger.php';
 require __DIR__ . '/Scout.php';
+require __DIR__ . '/Staff.php';
 
 $config_str = file_get_contents(__DIR__ . '/../config/config.json');
 $config = json_decode($config_str);
@@ -8,8 +9,11 @@ $config = json_decode($config_str);
 $scout = new Scout($config->targets);
 $result = $scout->explore();
 
+$staff = new Staff();
+$message = $staff->createReport($result);
+
 if (!in_array('--no-output', $argv)) {
     $messenger = new Messenger($config->channels);
-    $messenger->sendMessage($result);
+    $messenger->sendMessage($message);
 }
 
